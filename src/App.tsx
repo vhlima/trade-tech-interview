a915d1dc26fab897a6eaf7fcc56dead16ee35079
vhlima <vhlima2000@gmail.com>
2023-05-24 10:55:37 -0300

    feat: Implement new layout component on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PropsWithChildren } from 'react';
 
-const App: React.FC = () => {
+import Layout from './layout';
+
+const App: React.FC<PropsWithChildren> = props => {
+  const { children } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {children}    
+    </Layout>
   );
 }
 
