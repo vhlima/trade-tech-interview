d244db439cd486abd0f0de76b99852df5f515888
vhlima <vhlima2000@gmail.com>
2023-05-24 12:22:47 -0300

    feat: Add basic structure for SignIn page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import Layout from "../../layout";
+
+import './styles.css';
+
+const SignInPage: React.FC = () => {
+  return (
+    <Layout className="signin-page">
+      <div className="signin-container">
+      </div>
+    </Layout>
+  )
+}
+
+export default SignInPage;
