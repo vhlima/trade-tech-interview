9379a806edb71ca909dbccf1adefdfc2b909996f
vhlima <vhlima2000@gmail.com>
2023-05-24 16:12:57 -0300

    feat: Implement useSession hook

---
+++ src/hooks/useSession/index.tsx
@@ -0,0 +1,70 @@
+import { type PropsWithChildren, createContext, useContext, useState } from "react";
+
+import { fetchFromFootballApi } from "../../utils/fetchFromExternalSource";
+
+type SessionData = {
+  email: string;
+  accessToken: string;
+}
+
+type SignIn = (accessToken: string) => Promise<SessionData | undefined>;
+
+type SignInResponse = {
+  errors: [];
+  response: {
+    account: {
+      email: string;
+    }
+  }
+}
+
+interface SessionContextData {
+  session?: SessionData;
+  signIn: SignIn;
+}
+
+const SessionContext = createContext({} as SessionContextData);
+
+export const SessionProvider: React.FC<PropsWithChildren> = props => {
+  const { children } = props;
+
+  const [session, setSession] = useState<SessionData>();
+
+  const signIn: SignIn = async accessToken => {
+    const response = await fetchFromFootballApi(accessToken, '/status');
+
+    const data: SignInResponse = await response.json();
+
+    if(data.errors) {
+      throw new Error('An error occurred when signin in.');
+    }
+
+    const updatedSession = {
+      accessToken,
+      email: data.response.account.email,
+    }
+
+    setSession(updatedSession)
+
+    return updatedSession;
+  }
+
+  return (
+    <SessionContext.Provider value={{
+      session,
+      signIn,
+    }}>
+      {children}
+    </SessionContext.Provider>
+  )
+}
+
+export function useSession(): SessionContextData {
+  const context = useContext(SessionContext);
+
+  if(!context) {
+    throw new Error('useSession must be used within an provider.');
+  }
+
+  return context;
+}
