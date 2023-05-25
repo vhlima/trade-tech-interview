9facf34ecfe1334a74192b0388fb4f2f2851f051
vhlima <vhlima2000@gmail.com>
2023-05-25 09:48:27 -0300

    feat: Add Typography component

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,28 @@
+import type { ElementType, HtmlHTMLAttributes, PropsWithChildren } from 'react';
+
+import clsx from 'clsx';
+
+import './styles.css';
+
+interface Props extends HtmlHTMLAttributes<HTMLParagraphElement> {
+  component: ElementType;
+  color?: 'primary' | 'secondary';
+}
+
+export const Typography: React.FC<PropsWithChildren<Props>> = props => {
+  const { className, color = 'secondary', component, children, ...rest } = props;
+
+  const ElementComponent = component;
+
+  return (
+    <ElementComponent
+      className={clsx('typography', 
+        color,
+        className && className,
+      )}
+      {...rest}
+    >
+      {children}
+    </ElementComponent>
+  );
+};
