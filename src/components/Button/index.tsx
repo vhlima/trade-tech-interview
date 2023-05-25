64153f6a1bcb7b275280f57c9400e25b26927f7e
vhlima <vhlima2000@gmail.com>
2023-05-25 10:33:42 -0300

    chore: Incorporate fullWidth property into Typography component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,13 +6,18 @@ import './styles.css';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   intent?: 'primary';
+  fullWidth?: boolean;
 }
 
 const Button: React.FC<PropsWithChildren<Props>> = props => {
-  const { intent = 'primary', className, children, ...rest } = props;
+  const { intent = 'primary', fullWidth, className, children, ...rest } = props;
 
   return (
-    <button type="button" className={clsx('button', intent, className && className)} {...rest}>
+    <button type="button" className={clsx('button', intent, 
+      {
+        fullWidth,
+      }, 
+      className && className)} {...rest}>
       {children}
     </button>
   )
