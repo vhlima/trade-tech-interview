9888d87eb54de2f597bc9458c2993d4dad25717d
vhlima <vhlima2000@gmail.com>
2023-05-25 09:07:16 -0300

    feat: Implement SignInForm component on SignInPage

---
+++ src/pages/SignIn/components/Form/index.tsx
@@ -0,0 +1,50 @@
+import { type FormEvent, type HTMLAttributes, useState } from 'react';
+
+import Button from '../../../../components/Button';
+
+import { useSession } from '../../../../hooks/useSession';
+
+import { useErrorHandler } from '../../../../hooks/useErrorHandler';
+
+import './styles.css';
+
+type Props = Omit<HTMLAttributes<HTMLFormElement>, 'onSubmit'>;
+
+const SignInForm: React.FC<Props> = props => {
+  const [accessToken, setAccessToken] = useState<string>('');
+
+  const { signIn } = useSession();
+
+  const { errorMessage, handleError } = useErrorHandler();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await signIn(accessToken);
+    }catch(err) {
+      handleError(err);
+    }
+  }
+
+  return (
+    <form className="signin-form" onSubmit={handleSubmit} {...props}>
+      {errorMessage && (
+        <p className="form-error">{errorMessage}</p>
+      )}
+
+      <div className="signin-input-container">
+        <label htmlFor="accessToken">Access token</label>
+        <input id="accessToken" value={accessToken} onChange={e => setAccessToken(e.target.value)} data-testid="access-token-input" />
+
+        <small>You can retrieve the access token from your api-football account.</small>
+      </div>
+
+      <Button type="submit">
+        Sign in
+      </Button>
+    </form>
+  )
+}
+
+export default SignInForm;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,17 @@
 import Layout from "../../layout";
 
+import SignInForm from "./components/Form";
 import SignInHeader from './components/Header';
 
 import './styles.css';
 
 const SignInPage: React.FC = () => {
+
   return (
     <Layout className="signin-page">
       <div className="signin-container">
         <SignInHeader />
+        <SignInForm />
       </div>
     </Layout>
   )
